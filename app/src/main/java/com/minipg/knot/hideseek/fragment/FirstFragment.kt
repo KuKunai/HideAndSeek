b2b9529ee260fb68bedfb496b38ccd721b092e31
conaninw2 <se57160707@gmail,com>
2017-09-15 15:26:13 +0700

    สร้าง class   เพื่อเรียกใช้ Animetion

--- app/src/main/java/com/minipg/knot/hideseek/fragment/FirstFragment.kt
+++ app/src/main/java/com/minipg/knot/hideseek/fragment/FirstFragment.kt
@@ -1,7 +1,5 @@
 package com.minipg.knot.hideseek.fragment
 
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
 import android.app.Dialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
@@ -12,6 +10,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import com.minipg.knot.hideseek.LoveManagement
 import com.minipg.knot.hideseek.R
 import kotlinx.android.synthetic.main.customdialog.*
 import kotlinx.android.synthetic.main.fragment_one.*
@@ -76,30 +75,6 @@ class FirstFragment() : Fragment() {
 
     }
 
-    private fun setAnimation() {
-        val ran1 = (Math.random() * 100).toInt() + 0
-        val animSet1 = AnimatorSet()
-
-        val animX1 = ObjectAnimator.ofFloat(btnGG, View.SCALE_X, -3f)
-        val animY1 = ObjectAnimator.ofFloat(btnGG, View.SCALE_Y, 3f)
-        animSet1.playTogether(animX1, animY1)
-
-        val animSet2 = AnimatorSet()
-        val animX2 = ObjectAnimator.ofFloat(btnGG, View.SCALE_X, 1f)
-        val animY2 = ObjectAnimator.ofFloat(btnGG, View.SCALE_Y, 1f)
-        animSet2.playTogether(animX2, animY2)
-
-        val animSet3 = AnimatorSet()
-        val animX3 = ObjectAnimator.ofFloat(btnGG, View.SCALE_X, 0f)
-        val animY3 = ObjectAnimator.ofFloat(btnGG, View.SCALE_Y, 0f)
-        animSet3.playTogether(animX3, animY3)
-
-        val animSet = AnimatorSet()
-        animSet.playSequentially(animSet1, animSet2, animSet3)
-        animSet.duration = 500
-        animSet.start()
-    }
-
     fun custom_dialog() {
 
         val dialog = Dialog(context)
@@ -111,21 +86,12 @@ class FirstFragment() : Fragment() {
         dialog.textView2.setText("ได้รับ ♥ 1 ดวง")
 
         dialog.button1.setOnClickListener { ob ->
-//            setAnimation()
+            //            setAnimation()
+            val GG = LoveManagement(btnGG) //อนิเมชัน จาก class LoveManagement
+            GG.setAnimation()//อนิเมชัน จาก class LoveManagement
             dialog.cancel()
         }
         dialog.show()
     }
 
-
-//    private fun setRandomFragment() {
-//        val random = Random()
-//        randomFragmentNum = random.nextInt(2)
-//        when (randomFragmentNum) {
-//            0 -> R.layout.fragment_one
-//            1 -> R.layout.fragment_two
-//            2 -> R.layout.fragment_three
-//        }
-//
-//    }
 }
